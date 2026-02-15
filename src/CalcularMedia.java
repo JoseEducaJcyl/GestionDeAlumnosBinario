1177031f4cfada0a29225e6b60d292e37f6b30a4
JoseEducaJcyl <josea.morgut.1@educa.jcyl.es>
2026-02-15 18:42:25 +0100

    Comentarios agregados

--- src/CalcularMedia.java
+++ src/CalcularMedia.java
@@ -1,12 +1,19 @@
+// CalcularMedia.java
 import java.util.ArrayList;
 
 public class CalcularMedia {
+    // Este método calcula la nota media de todos los estudiantes de la lista
     public static double calcularMedia(ArrayList<Estudiante> estudiantes) {
-        double media = 0;
+        double media = 0; // Variable para ir acumulando las notas
+        
+        // Recorro todos los estudiantes de la lista
         for (Estudiante estudiante : estudiantes) {
-            media += estudiante.getNota();
+            media += estudiante.getNota(); // Voy sumando las notas
         }
+        
+        // Divido la suma total entre el número de estudiantes
         media = media / estudiantes.size();
-        return media;
+        
+        return media; // Devuelvo la media calculada
     }
 }

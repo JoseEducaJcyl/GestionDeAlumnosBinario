35bdb5f8ca0864007803e0515a494bfb03b1d2ed
JoseEducaJcyl <josea.morgut.1@educa.jcyl.es>
2026-02-15 18:43:53 +0100

    Comentarios agregados

--- src/GeneradorDatos.java
+++ src/GeneradorDatos.java
@@ -1,13 +1,15 @@
+// GeneradorDatos.java
 import java.util.ArrayList;
 
 public class GeneradorDatos {
+    // Este método añade estudiantes de ejemplo a la lista que recibe
     public static ArrayList<Estudiante> crearEstudiantes(ArrayList<Estudiante> estudiantes){
+        // Creo 4 estudiantes con sus nombres y notas y los añado a la lista
         estudiantes.add(new Estudiante("Juan", 7.5));
         estudiantes.add(new Estudiante("Maria", 8.2));
         estudiantes.add(new Estudiante("Pedro", 6.9));
         estudiantes.add(new Estudiante("Ana", 9.1));
 
-        return estudiantes;
-
+        return estudiantes; // Devuelvo la misma lista con los estudiantes añadidos
     }
 }

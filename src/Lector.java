65abd227dbeacd2a771028f5d7169d504c4d0d3c
JoseEducaJcyl <josea.morgut.1@educa.jcyl.es>
2026-02-15 18:44:16 +0100

    Comentarios agregados

--- src/Lector.java
+++ src/Lector.java
@@ -1,15 +1,22 @@
+// Lector.java
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
 import java.util.ArrayList;
 
 public class Lector {
+    // Este método lee una lista de estudiantes desde un archivo binario
     public static ArrayList<Estudiante> leerEstudiantes(String ruta){
+        // Creo una lista vacía donde guardaré los estudiantes leídos
         ArrayList<Estudiante> estudiantes = new ArrayList<>();
+        
+        // Uso ObjectInputStream para leer objetos completos del archivo
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(ruta))){
+            // Leo el objeto del archivo y lo convierto (casteo) a ArrayList<Estudiante>
             estudiantes = (ArrayList<Estudiante>) ois.readObject();
         }catch(Exception e){
+            // Si hay cualquier error, lanzo una excepción con el mensaje
             throw  new RuntimeException("Error al leer: " + e);
         }
-        return estudiantes;
+        return estudiantes; // Devuelvo la lista con los estudiantes leídos
     }
 }
